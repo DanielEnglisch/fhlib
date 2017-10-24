d8318ccbac7973d25a18bf0816ac180ffcb55fe6
Unknown <andi.roither@hotmail.de>
2017-10-24 13:21:30 +0200

    ArrayUtils
    Added Comments and some Error Handling

--- src/ArrayUtils/ArrayUtils.c
+++ src/ArrayUtils/ArrayUtils.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// allocate mulit array with 0 as default value
 int** AllocateMultiarray(unsigned int RowNum)
 {
-    int **a = malloc(sizeof *a * RowNum);
+    int **a = malloc(sizeof(*a) * RowNum);
 
     if (a)
     {
         for (unsigned int i = 0; i < RowNum; i++)
         {
-            a[i] = malloc(sizeof(*a[i]) * RowNum);
+            a[i] = malloc(sizeof(*(a[i])) * RowNum);
         }
 
         for (unsigned int i = 0; i < RowNum; i++)
@@ -21,35 +22,64 @@ int** AllocateMultiarray(unsigned int RowNum)
             }
         }
     }
+    else
+    {
+        printf("\n-ERROR-\nAllocateMultiArray: Failed to allocate ressources");
+    }
 
     return a;
 }
 
-int** AllocateMultiarrayConvert(unsigned int RowNum, int *arr)
+// allocate multi array from existing one dimensional array
+int** AllocateMultiarrayConvert(unsigned int RowNum, int arr[])
 {
-    int **a = malloc(sizeof *a * RowNum);
+    // only for static Arrays, not dynamic ones
+    int length = sizeof(arr[0]);
     int count = 0;
+    int **a = malloc(sizeof(*a) * RowNum);
 
     if (a)
     {
         for (unsigned int i = 0; i < RowNum; i++)
         {
-            a[i] = malloc(sizeof(*a[i]) * RowNum);
+            a[i] = malloc(sizeof(*(a[i])) * RowNum);
         }
 
         for (unsigned int i = 0; i < RowNum; i++)
         {
             for (unsigned int j = 0; j < RowNum; j++)
             {
-                a[i][j] = arr[count];
-                count++;
+                if (count < length)
+                {
+                    a[i][j] = arr[count];
+                    count++;
+                }
+                else
+                {
+                    a[i][j] = arr[count];
+                }
             }
         }
+    }
+    else
+    {
+        printf("\n-ERROR-\nAllocateMultiArrayConvert: Failed to allocate ressources");
     }
 
     return a;
 }
 
+// Free the allocated multi array
+void FreeMultiArray(int **a, unsigned int RowNum)
+{
+    for (int i = 0; i < RowNum; i++)
+    {
+        free(a[i]);
+    }
+    free(a);
+}
+
+// fill the multi array with random numbers below the scope
 void FillMultiArrayRandom(int **a, int RowNum, int scope)
 {
     for (int i = 0; i < RowNum; i++)
@@ -61,6 +91,7 @@ void FillMultiArrayRandom(int **a, int RowNum, int scope)
     }
 }
 
+// print the multi array to the console
 void PrintMultiArray(int **a, int RowNum)
 {
     for (int i = 0; i < RowNum; i++)
