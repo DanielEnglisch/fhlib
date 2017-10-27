8f6cb1766219dd6cde5908dd08d0e9fef5e0a281
Andreas Roither <andi.roither@hotmail.de>
2017-10-27 22:15:28 +0200

    ArrayUtils
    Bug fixes, comments

--- src/ArrayUtils/ArrayUtils.c
+++ src/ArrayUtils/ArrayUtils.c
@@ -1,34 +1,52 @@
+/*C**********************************************************************
+* FILENAME :        ArrayUtils.c
+*
+* DESCRIPTION :
+*       Converts 1D Array into 2D Array +
+*       utility functions
+*
+* NOTES :
+*       
+*
+* 
+* AUTHOR :    Andreas Roither        START DATE :    27.10.17
+*
+* CHANGES :
+*
+*C*/
+
 #include "ArrayUtils.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 // allocate mulit array with 0 as default value
-int **AllocateMultiarray(unsigned int RowNum)
+int** AllocateMultiarray(unsigned int RowNum)
 {
-
     int **a = malloc(sizeof(*a) * RowNum);
 
     if (a)
     {
-
         for (unsigned int i = 0; i < RowNum; i++)
+        {
             a[i] = malloc(sizeof(*(a[i])) * RowNum);
+        }
 
         for (unsigned int i = 0; i < RowNum; i++)
+        {
             for (unsigned int j = 0; j < RowNum; j++)
+            {
                 a[i][j] = 0;
+            }
+        }
     }
     else
+    {
         printf("\n-ERROR-\nAllocateMultiArray: Failed to allocate ressources");
-
+    }
     return a;
 }
 
 // allocate multi array from existing one dimensional array
-int **AllocateMultiarrayConvert(unsigned int RowNum, int arr[])
+int** AllocateMultiarrayConvert(unsigned int RowNum, int arr[])
 {
-    // only for static Arrays, not dynamic ones
-    int length = sizeof(arr[0]);
     int count = 0;
     int **a = malloc(sizeof(*a) * RowNum);
 
@@ -43,15 +61,8 @@ int **AllocateMultiarrayConvert(unsigned int RowNum, int arr[])
         {
             for (unsigned int j = 0; j < RowNum; j++)
             {
-                if (count < length)
-                {
-                    a[i][j] = arr[count];
-                    count++;
-                }
-                else
-                {
-                    a[i][j] = arr[count];
-                }
+                a[i][j] = arr[count];
+                count++;
             }
         }
     }
@@ -59,7 +70,6 @@ int **AllocateMultiarrayConvert(unsigned int RowNum, int arr[])
     {
         printf("\n-ERROR-\nAllocateMultiArrayConvert: Failed to allocate ressources");
     }
-
     return a;
 }
 
@@ -74,11 +84,11 @@ void FreeMultiArray(int **a, unsigned int RowNum)
 }
 
 // fill the multi array with random numbers below the scope
-void FillMultiArrayRandom(int **a, int RowNum, int scope)
+void FillMultiArrayRandom(int **a, unsigned int RowNum, int scope)
 {
-    for (int i = 0; i < RowNum; i++)
+    for (unsigned int i = 0; i < RowNum; i++)
     {
-        for (int j = 0; j < RowNum; j++)
+        for (unsigned int j = 0; j < RowNum; j++)
         {
             a[i][j] = rand() % scope;
         }
@@ -86,11 +96,11 @@ void FillMultiArrayRandom(int **a, int RowNum, int scope)
 }
 
 // print the multi array to the console
-void PrintMultiArray(int **a, int RowNum)
+void PrintMultiArray(int **a, unsigned int RowNum)
 {
-    for (int i = 0; i < RowNum; i++)
+    for (unsigned int i = 0; i < RowNum; i++)
     {
-        for (int j = 0; j < RowNum; j++)
+        for (unsigned int j = 0; j < RowNum; j++)
         {
             printf("%d ", a[i][j]);
         }
