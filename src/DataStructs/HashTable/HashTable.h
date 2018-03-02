0fcc27de185fe3c8870782c134b5646b1bd52dde
Abrynos <6608231+Abrynos@users.noreply.github.com>
2018-03-02 22:21:40 +0100

    general hash function for all types
    created general hash-function for ALL possible Types. don't try to understand it. really not worth it. believe me.

--- src/DataStructs/HashTable/HashTable.h
+++ src/DataStructs/HashTable/HashTable.h
@@ -2,14 +2,17 @@
 #define HASHTABLE_H
 
 #include <list>
+#include <bitset>
 #include <iostream>
+#include <limits>
+
+typedef std::bitset<8> BYTE;
 
 template<typename T>
 class HashTable{
     public:
-        HashTable(int (*fkt)(const T&), int size = 5000)
+        HashTable(int size = 5000)
         : _size {size}
-        , hashFunction {fkt}
         {
             table = new std::list<T>[_size];
         };
@@ -33,11 +36,28 @@ class HashTable{
         int Count(){
             return count;
         };
+        int hashFunction(const T& val){
+            BYTE* byteArray = (BYTE*)(&val);
+            int size = sizeof(T);
+            int* intArray = (int*)byteArray;
+            int intSize = size / sizeof(int);
+            int extraBytes = size % sizeof(int);
+            int hash = std::numeric_limits<int>::max();
+            for(int i = 0; i < intSize; i++){
+                hash ^= intArray[i];//hash = hash XOR intArray[i]
+            }
+            if(extraBytes > 0){
+                for(int i = 0; i < extraBytes; i++){
+                    BYTE* lastByteOfHash = ((BYTE*)(&(hash))) + sizeof(int) - 1;
+                    *lastByteOfHash = *lastByteOfHash ^ byteArray[intSize * sizeof(int) + i];
+                }
+            }
+            return hash;
+        };
     private:
         int _size;
         int count {0};
         std::list<T>* table;
-        int (*hashFunction)(const T&);
 };
 
 #endif
